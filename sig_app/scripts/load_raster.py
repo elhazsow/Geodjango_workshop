bd18d1a262b2920720c4d5c337838e1cb4cce335
El hadji sow <sowelhaz@gmail.com>
2026-07-20 01:59:09 +0200

    add raster models

---
+++ sig_app/scripts/load_raster.py
@@ -0,0 +1,11 @@
+from sig_app.models import Raster
+from django.contrib.gis.gdal import GDALRaster
+
+
+def run(verbose=True):
+    print("Chargement Raster DTM")
+    dtmDk=r"D:\téléchargement\DTM_dakar\sow\DKR.tif"
+    rast=GDALRaster(dtmDk, write=True)
+    raster=Raster(name="DTM Dakar", rst=rast)
+    raster.save()
+    print("Image raster chargée:", raster.name)
